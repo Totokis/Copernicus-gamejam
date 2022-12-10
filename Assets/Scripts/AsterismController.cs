447db476222b8ff16e3ca32e6f6c319adad4154b
Michał Minikowski <perconey@gmail.com>
2022-12-10 20:59:33 +0100

    mokar

--- Assets/Scripts/AsterismController.cs
+++ Assets/Scripts/AsterismController.cs
@@ -69,13 +69,30 @@ public class AsterismController : MonoBehaviour
             new AsterismPathInfo(MoveDirection.RightDown, new List<MoveDirection>(){ MoveDirection.LeftUp}),
         },
 
+        new List<AsterismPathInfo>()
+        {
+            new AsterismPathInfo(MoveDirection.LeftUp, new List<MoveDirection>(){ MoveDirection.RightDown, MoveDirection.Up}),
+            new AsterismPathInfo(MoveDirection.Up, new List<MoveDirection>(){ MoveDirection.Down}),
+            new AsterismPathInfo(MoveDirection.Down, new List<MoveDirection>(){ MoveDirection.RightDown, MoveDirection.Up}),
+            new AsterismPathInfo(MoveDirection.RightDown, new List<MoveDirection>(){ MoveDirection.LeftUp, MoveDirection.RightUp, MoveDirection.Down}),
+            //znowu pocz
+            new AsterismPathInfo(MoveDirection.Down, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.LeftDown, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.Down, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.LeftDown, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.LeftUp, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.LeftDown, new List<MoveDirection>(){ }),
+            new AsterismPathInfo(MoveDirection.LeftUp, new List<MoveDirection>(){ }),
+
+        },
+
     };
 
     public List<AsterismPathInfo> CurrentAsterism;
 
     void Start()
     {
-        CurrentAsterism = asterisms[0];
+        CurrentAsterism = asterisms[3];
 
         HexGridGenerator.Instance.SpawnMapAt(trThoughtParent.position, trThoughtParent, true);
     }
