ee6164a5eb44cac923024546a15eca5b1ae5ea75
Michał Minikowski <perconey@gmail.com>
2022-12-10 19:03:35 +0100

    ergerg

--- Assets/Scripts/HexNode.cs
+++ Assets/Scripts/HexNode.cs
@@ -21,19 +21,34 @@ public class HexNode : MonoBehaviour
         VisitsFromWhereDirections = new List<MoveDirection>();
 
         GetComponent<SpriteRenderer>().color = Color.HSVToRGB(51f / 360f, UnityEngine.Random.Range(21f, 46f) / 100f, 1f);
+
     }
 
-    public void AddVisit(MoveDirection visit)
+    private void EffectOnAppearing()
     {
-        if (VisitsFromWhereDirections.Count == 0)
-        {
-            LeanTween.scale(gameObject, Vector3.one, UnityEngine.Random.Range(0.70f, 0.94f))
+        Single start = UnityEngine.Random.Range(0f, 360f);
+        LeanTween.value(start, start + UnityEngine.Random.Range(90f, 120f) * (UnityEngine.Random.value < 0.5f ? 1f : -1f), UnityEngine.Random.Range(0.9f, 1.4f))
+            .setOnUpdate((Single val) =>
+            {
+                transform.localEulerAngles = new Vector3(0f, 0f, val);
+            })
+            .setEaseOutSine();
+
+        LeanTween.scale(gameObject, Vector3.one, UnityEngine.Random.Range(0.70f, 0.94f))
                 .setEaseInOutBack()
                 .setOnComplete(() =>
                 {
                     LeanTween.scale(gameObject, new Vector3(0.8f, 0.8f), UnityEngine.Random.Range(0.6f, 1.2f))
                         .setLoopPingPong();
                 });
+
+    }
+
+    public void AddVisit(MoveDirection visit)
+    {
+        if (VisitsFromWhereDirections.Count == 0)
+        {
+            EffectOnAppearing();
         }
         VisitsFromWhereDirections.Add(visit);
     }
